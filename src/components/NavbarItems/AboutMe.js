45600ddea5c414cbd246d4505b8f98e8bc251591
cartwheei <20185181008@ogr.akdeniz.edu.tr>
2021-11-07 22:37:44 +0300

    add email

--- src/components/NavbarItems/AboutMe.js
+++ src/components/NavbarItems/AboutMe.js
@@ -3,6 +3,7 @@ import { Accordion } from "react-bootstrap";
 import * as IoIcons from "react-icons/io";
 import * as BsIcons from "react-icons/bs";
 import * as Cgicons from "react-icons/cg";
+import * as Gricons from "react-icons/gr";
 import "./AboutMe.css";
 
 function AboutMe() {
@@ -15,39 +16,48 @@ function AboutMe() {
         </div>
       </Accordion.Header>
       <Accordion.Body>
-        <div className="aboutme-accordion-body">
-          <a
-            title="LinkedIn"
-            href="https://www.linkedin.com/in/alper-emek-362520159/"
-            target="_blank"
-            rel="noreferrer"
-          >
-            <BsIcons.BsLinkedin className="about-me-icon"></BsIcons.BsLinkedin>
-          </a>
-          <a
-            title="Fork Me On Github!"
-            target="_blank"
-            rel="noreferrer"
-            href="https://github.com/cartwheei/Istanbul-Deprem-Haritasi"
-          >
-            <BsIcons.BsGithub className="about-me-icon"></BsIcons.BsGithub>
-          </a>
-          <a
-            title="Portfolio"
-            href="https://alperemek.netlify.app/"
-            target="_blank"
-            rel="noreferrer"
-          >
-            <Cgicons.CgWebsite className="about-me-icon"></Cgicons.CgWebsite>
-          </a>
-          <a
-            title="Twitter"
-            href="https://twitter.com/AlperEmek"
-            target="_blank"
-            rel="noreferrer"
-          >
-            <BsIcons.BsTwitter className="about-me-icon"></BsIcons.BsTwitter>
-          </a>
+        <div className="aboutme-accordion-body-wrapper">
+          <div className="aboutme-accordion-body">
+            <a
+              title="LinkedIn"
+              href="https://www.linkedin.com/in/alper-emek-362520159/"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <BsIcons.BsLinkedin className="about-me-icon"></BsIcons.BsLinkedin>
+            </a>
+            <a
+              title="Fork Me On Github!"
+              target="_blank"
+              rel="noreferrer"
+              href="https://github.com/cartwheei/Istanbul-Deprem-Haritasi"
+            >
+              <BsIcons.BsGithub className="about-me-icon"></BsIcons.BsGithub>
+            </a>
+            <a
+              title="Portfolio"
+              href="https://alperemek.netlify.app/"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <Cgicons.CgWebsite className="about-me-icon"></Cgicons.CgWebsite>
+            </a>
+            <a
+              title="Twitter"
+              href="https://twitter.com/AlperEmek"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <BsIcons.BsTwitter className="about-me-icon"></BsIcons.BsTwitter>
+            </a>
+            <a
+              title="recaialperemek@gmail.com"
+              href="mailto:recaialperemek@gmail.com"
+              rel="noreferrer"
+            >
+              <Gricons.GrMail className="about-me-icon"></Gricons.GrMail>
+            </a>
+          </div>
         </div>
       </Accordion.Body>
     </Accordion.Item>
