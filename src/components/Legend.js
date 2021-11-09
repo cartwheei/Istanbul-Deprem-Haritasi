d1ce4741bcc4a9b0a46d4cf3b640af9ddabd1c20
acargorkem <acargorkem@outlook.com>
2021-11-09 17:36:11 +0300

    Fix Legends.js and Navbar.css for eslint warnings

--- src/components/Legend.js
+++ src/components/Legend.js
@@ -1,29 +1,28 @@
-
 import React from "react";
-import './Legend.css'
+import "./Legend.css";
 
-
-const Legend = () =>{
-
+const Legend = () => {
   const layer = [
-    {name:'Ags: Çok Yüksek Risk', color:'#ee1112'},
-    {name:'Bgs: Yüksek Risk', color:'#deeb14'},
-    {name:'Cgs: Orta Risk', color:'#19e641'}
-  ]
+    { name: "Ags: Çok Yüksek Risk", color: "#ee1112" },
+    { name: "Bgs: Yüksek Risk", color: "#deeb14" },
+    { name: "Cgs: Orta Risk", color: "#19e641" },
+  ];
 
-
-  return(
+  return (
     <div className="legend">
       <h4>Harita Lejandı</h4>
-      {layer.map((layer)=>(
-        <div>
-        <span id='risk' style={{backgroundColor:layer.color}} key={layer.name}></span>
-        {layer.name}
+      {layer.map((layer, index) => (
+        <div key={index}>
+          <span
+            id="risk"
+            style={{ backgroundColor: layer.color }}
+            key={layer.name}
+          ></span>
+          {layer.name}
         </div>
       ))}
-      
     </div>
-  )
-}
+  );
+};
 
 export default Legend;
